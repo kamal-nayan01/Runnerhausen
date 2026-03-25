e49d265a5fec50d156c8fc13ecc4fbab1756f07a
Kamal Nayan <266915618+kamal-nayan01@users.noreply.github.com>
2026-03-25 09:30:38 +0530

    Add service worker for offline support and caching

---
+++ sw.js
@@ -0,0 +1,36 @@
+// sw.js - Service Worker for Offline Support and Caching
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== 'my-cache') {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
